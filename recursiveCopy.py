285bc95f73809045a6700f81aa4f4686cd33d632
osirisf <f.rossi57@studenti.unipi.it>
2015-07-13 22:23:17 +0200

    Update recursiveCopy.py
    Fixed force close due to PermissionError

--- recursiveCopy.py
+++ recursiveCopy.py
@@ -2,25 +2,43 @@ import shutil
 import os
 
 
-def recursiveList(road,dst,ignored):
-	dirs = os.listdir(road)
-	for adir in dirs:
-		curr = road+'/'+adir
+def recursiveList(road,dst,ignored,ext):
+	dirs = os.listdir(road)  #list files or directories in root directory
+	for adir in dirs:   
+		curr = road+'/'+adir #get path
 
 		if adir in ignored:
 			continue
 
-		if(os.path.isdir(curr)):
-			recursiveList(curr,dst,ignored)
+		if(os.path.isdir(curr)):  #if directory recursive access it
+			try:
+				recursiveList(curr,dst,ignored,ext)
+			except(PermissionError) as e:
+				continue
+		
 		else:
-			fname,fnext = os.path.splitext(curr)
-			if(fnext == '.pdf'):
-				shutil.copy(curr,dst+os.path.basename(curr))
+			fname,fnext = os.path.splitext(curr) #get filename and extension
+			if(fnext in ext): 
+				print("Copied: "+curr,end='\r')
+
+				try:
+					shutil.copy(curr,dst+os.path.basename(curr))
+				except(shutil.SameFileError) as er:
+					continue
+			else:
+				print("Skipped: "+curr,end='\r')
 
 
 
-ignore = {'cp',};
-recursiveList('/home','cp/',ignore);
+ignore = {'cp','sys','dev','bin'}
+srch = {'.pdf','.conf'}
+
+try:
+	os.makedirs("recpy");
+except(FileExistsError) as e:
+	print('') 	
+
+recursiveList('/','recpy/',ignore,srch)
 
 
 
