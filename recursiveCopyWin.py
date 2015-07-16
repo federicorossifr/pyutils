6eddb6fadd2390474a4def15b2e3c132c9d83dc6
Federico Rossi <rossirule2012@gmail.com>
2015-07-16 08:00:47 +0100

    Up
    upd

--- recursiveCopyWin.py
+++ recursiveCopyWin.py
@@ -1,7 +1,7 @@
 import shutil
 import os
 import sys
-
+import win32api
 
 def recursiveList(road,dst,ignored,ext):
 	dirs = os.listdir(road)  #list files or directories in root directory
@@ -30,16 +30,17 @@ def recursiveList(road,dst,ignored,ext):
 				print("Skipped: "+curr,end='\n')
 
 
-start = "C:\\"
-ignore = {'Windows'} #filter folder and files
+start = win32api.GetLogicalDriveStrings()
+start = start.split('\000')[:-1]
+ignore = {'Windows','Program Files','Program Files (x86)'} #filter folder and files
 srch = {'.pdf'} #filter files extensions
 
 try:
 	os.makedirs("recpy");
 except(FileExistsError) as e:
 	print('') 	
-
-recursiveList(start,'recpy/',ignore,srch)
+for s in start:
+	recursiveList(s,'recpy/',ignore,srch)
 
 
 
